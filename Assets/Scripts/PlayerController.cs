5aefec84128087c64de70f83eaf174d2cbaf37d3
RocketMonent <skywarsminedrack@gmail.com>
2024-02-16 16:18:28 +0100

    add logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public GameObject food;
+    private float xRange = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,22 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+      
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, 0, 0);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 offset = new Vector3(0, 1.5f, 1f);
+            Vector3 foodSpawnPosition = transform.position + offset;
+
+            Instantiate(food, foodSpawnPosition, food.transform.rotation);
+        }
     }
 }
