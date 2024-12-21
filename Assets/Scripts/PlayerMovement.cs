33e776d26a36b294109ed877af40b98c1f95abf7
PEK <skywarsminedrack@gmail.com>
2024-12-21 11:59:57 +0100

    PlayerMovement (Input)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 movementDirection;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        movementDirection.Set(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+    }
+}
