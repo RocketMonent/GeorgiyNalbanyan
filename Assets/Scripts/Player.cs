2645a3026c68e2487b3d217509cd77d630540816
RocketMonent <>
2025-10-15 17:32:27 +0300

    PlayerVisual + Rotation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+    private void Update()
+    {
+        Vector2 inputVector2 = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector2.y = +1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector2.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector2.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector2.x = +1;
+        }
+
+        inputVector2 = inputVector2.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector2.x, 0, inputVector2.y);
+
+        transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+
+    }
+}
