7d2fc0c10cef2c456cc42042ae9fe969c28a21f3
RocketMonent <>
2025-11-05 18:44:20 +0300

    Colision Settings

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,7 +22,34 @@ public class Player : MonoBehaviour
 
         isWalking = moveDirection != Vector3.zero;
 
-        transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerRadius,playerRadius, moveDirection, moveDistance);
+
+        if (canMove == false)
+        {
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerRadius, playerRadius, moveDirectionX, moveDistance);
+            if (canMove == true)
+            {
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position = Vector3.up * playerRadius, playerRadius, moveDirectionZ, moveDistance);
+                if (canMove == true)
+                {
+                    moveDirection = moveDirectionZ;
+                }
+            }
+        }
+        
+        if (canMove == true)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
 
